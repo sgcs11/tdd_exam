ed2313e2958b4b679695b66f66cc4376879f0d12
sgcs11 <joachy@naver.com>
2022-07-05 11:50:37 +0900

    테스트 케이스 2개, 더하기 기능

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -8,7 +8,15 @@ public class AppTest {
 
     @Test
     public void 더하기_테스트() {
-        int a = 10 + 5;
-        assertEquals(15, a);
+        int rs = Calculator.add("10 + 20");
+
+        assertEquals(30, rs);
+    }
+
+    @Test
+    public void 더하기_테스트_2() {
+        int rs = Calculator.add("20 + 20");
+
+        assertEquals(40, rs);
     }
 }
